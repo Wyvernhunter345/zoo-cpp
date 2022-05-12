93a037a173f69ea7162e57c9ede5b226e5eca481
bobthellorma <98739156+bobthellorma@users.noreply.github.com>
2022-05-12 14:49:00 +0930

    to save 12/5

---
+++ remove.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    // user has hit 'R' to remove an animal
+
+    // get name of animal to remove
+    string response;
+    cout << "What is the name of the animal you wish to remove?" << endl;
+    cin >> response;
+    cout >> response >> endl;
+
+
+
+}
